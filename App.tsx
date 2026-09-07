b1ca7c914ea4e29a98efc015d1f945d2686bd2f1
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-09-08 03:45:35 +0530

    Wrap AppNavigator with FirebaseProvider for improved context management

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AppNavigator } from './src/navigation/AppNavigator';
+import { FirebaseProvider } from './src/providers/FirebaseProvider';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <StatusBar style="dark" />
-      <AppNavigator />
+      <FirebaseProvider><AppNavigator /></FirebaseProvider>
     </SafeAreaProvider>
   );
 }
