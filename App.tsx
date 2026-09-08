e3742943d1b4b7c120e4c14a9dbf3d0979389fff
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-09-08 17:34:16 +0530

    Add splash screen configuration to App component

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AppNavigator } from './src/navigation/AppNavigator';
 import { FirebaseProvider } from './src/providers/FirebaseProvider';
+import * as SplashScreen from 'expo-splash-screen';
+void SplashScreen.preventAutoHideAsync().catch(() => {});
+SplashScreen.setOptions({ duration: 350, fade: true });
 
 export default function App() {
   return (
